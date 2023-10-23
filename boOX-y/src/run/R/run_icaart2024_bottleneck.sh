59cf747a97d6911d84d5fe791ee851d479470f21
Tomáš Kolárik <kolarto5@fit.cvut.cz>
2023-10-23 14:56:31 +0200

    extended bottleneck experiments to also ccbs and lra

--- boOX-y/src/run/R/run_icaart2024_bottleneck.sh
+++ boOX-y/src/run/R/run_icaart2024_bottleneck.sh
@@ -3,27 +3,70 @@
 [[ -z $MIN_K ]] && MIN_K=2
 [[ -z $MAX_K ]] && MAX_K=10
 
-BIN=../../main/mapfR_solver_boOX
+TOOLS=(boox ccbs lra)
+
+function exec_boox {
+    ../../main/mapfR_solver_boOX --input-mapR-file=bottleneck/bottleneck_${k}.mapR --input-kruhoR-file=bottleneck/bottleneck_${k}.kruR --algorithm='smtcbsR*'
+}
+
+function exec_ccbs {
+    ../../../../ccbs/CCBS bottleneck/bottleneck_map_${k}.xml bottleneck/bottleneck_task_${k}.xml ../../../../ccbs/Examples/config_bottleneck.xml
+}
+
+function exec_lra {
+    ../../../../mapf_r/bin/release/mathsat_solver bottleneck/bottleneck_${k}.mapR bottleneck/bottleneck_${k}.kruR
+}
+
+function extract_t_boox {
+    sed -rn 's/^.*Wall clock TIME[^=]+= (.*)$/\1/p' | tail -n 1
+}
+
+function extract_t_ccbs {
+    sed -rn 's/^Runtime: (.*)$/\1/p'
+}
+
+function extract_t_lra {
+    sed -rn 's/^main duration: ([0-9.]+) s$/\1/p'
+}
+
+function failed_boox {
+    ! grep -q 'SOLVABLE' $ofile || grep -q 'FAILED' $ofile
+}
+
+function failed_ccbs {
+    grep -q 'found: false' $ofile
+}
+
+function failed_lra {
+    ! grep -q 'guaranteed suboptimal coefficient' $ofile
+}
 
 DATA_FILE=./bottleneck.dat
 
-printf "k\tt\n" >$DATA_FILE
+for tool in ${TOOLS[@]}; do
+    printf "%s ...\n" $tool
 
-for (( k=$MIN_K; $k <= $MAX_K; ++k )); do
-    ofile=bottleneck/out_${k}
+    bin=${BINS[$tool]}
 
-    printf "k=%d ..." $k
-    $BIN --input-mapR-file=bottleneck/bottleneck_${k}.mapR --input-kruhoR-file=bottleneck/bottleneck_${k}.kruR --algorithm='smtcbsR*' >$ofile || exit $?
-    printf "\n"
+    data_file=${DATA_FILE%.dat}_${tool}.dat
+    printf "k\tt\n" >$data_file
 
-    t=$(sed -rn 's/^.*Wall clock TIME[^=]+= (.*)$/\1/p' <$ofile | tail -n 1)
-    printf "%d\t%.4f" $k $t >>$DATA_FILE
+    for (( k=$MIN_K; $k <= $MAX_K; ++k )); do
+        ofile=bottleneck/out_${tool}_${k}.txt
 
-    ( ! grep -q 'SOLVABLE' $ofile || grep -q 'FAILED' $ofile ) && {
-        printf "\tX" >>$DATA_FILE
-    }
+        printf "k=%d ..." $k
+        exec_${tool} >$ofile || exit $?
+        printf "\n"
 
-    printf "\n" >>$DATA_FILE
+        t=$(extract_t_${tool} <$ofile)
+        printf "%d\t%.4f" $k $t >>$data_file
+
+        failed_${tool} && {
+            printf "\tX" >>$data_file
+        }
+
+        printf "\n" >>$data_file
+    done
 done
 
 printf "Done.\n"
